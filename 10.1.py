8ed9dfd37b1980017a2c8680299d0c7a559aa66d
Khoa hoc du lieu 17a3 <ntkhoi.dhkl17a3hn@sv.uneti.edu.vn>
2024-01-12 11:19:20 +0700

    add file

---
+++ 10.1.py
@@ -0,0 +1,8 @@
+with open("HumptyDumty.txt","w",encoding='utf-8') as f:
+    f.write("Nội dung tập tin:\n")
+    f.write("Humpty Dumpty sat on a wall,\n")
+    f.write("Humpty Dumpty had a great fall \n")
+    f.write("All the king's horses and all the king's men\n")
+    f.write("Couldn't put Humpty together again")
+f.close()
+
---
+++ 10.3.py
@@ -0,0 +1,4 @@
+with open("Rain.txt","w",encoding='utf-8') as f:
+    f.write("Rain rain, go away; Come again another day....:\n")
+   
+f.close()
